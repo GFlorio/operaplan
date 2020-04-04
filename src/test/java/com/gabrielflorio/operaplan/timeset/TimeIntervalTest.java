bd829a567ac42ebdc5ec9685246389a5e931fc3d
Gabriel Florio <gabriel@gabrielflorio.com>
2020-04-04 15:31:52 -0300

    Add a few tests

---
+++ src/test/java/com/gabrielflorio/operaplan/timeset/TimeIntervalTest.java
@@ -0,0 +1,40 @@
+package com.gabrielflorio.operaplan.timeset;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeIntervalTest {
+
+    @Test
+    void between() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void overlapsWith() {
+    }
+
+    @Test
+    void isSubset() {
+    }
+
+    @Test
+    void duration() {
+    }
+
+    @Test
+    void translatedBy() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void end() {
+    }
+}
---
+++ src/test/java/com/gabrielflorio/operaplan/timeset/TimeSetTest.java
@@ -0,0 +1,54 @@
+package com.gabrielflorio.operaplan.timeset;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TimeSetTest {
+
+    @Test
+    void of() {
+    }
+
+    @Test
+    void ofInterval() {
+    }
+
+    @Test
+    void union() {
+    }
+
+    @Test
+    void intersection() {
+    }
+
+    @Test
+    void difference() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void isSubset() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void limitingInterval() {
+    }
+
+    @Test
+    void duration() {
+    }
+
+    @Test
+    void intervals() {
+    }
+}
